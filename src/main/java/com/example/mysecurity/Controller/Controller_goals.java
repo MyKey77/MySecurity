6552179b6cb51b3f59613367213d6f5a8f287483
Mikey <mikey.mazza1999@gmail.com>
2023-03-19 20:24:15 +0100

    change

--- src/main/java/com/example/mysecurity/Controller/Controller_goals.java
+++ src/main/java/com/example/mysecurity/Controller/Controller_goals.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller_goals {
-    @GetMapping(path = {"/","/home"})
+    @GetMapping(path = {"/","/testing"})
     public String homepage(){return "testing";}
 
 
