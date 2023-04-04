16b5fb686edf54cecb5c572404b1c2d6a2dacd81
Mikey <mikey.mazza1999@gmail.com>
2023-04-04 11:00:06 +0200

    change

---
+++ src/main/java/com/example/mysecurity/controllerDbSc/ControllerGoals.java
@@ -0,0 +1,14 @@
+package com.example.mysecurity.controllerDbSc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerGoals {
+    @GetMapping(path = {"/","/testing"})
+    public String homepage(){return "testing";}
+
+
+
+}
---
+++ src/main/java/com/example/mysecurity/controllerDbSc/RegistrationForm.java
@@ -0,0 +1,14 @@
+package com.example.mysecurity.controllerDbSc;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RegistrationForm {
+
+    private String name;
+    private String password;
+
+}
